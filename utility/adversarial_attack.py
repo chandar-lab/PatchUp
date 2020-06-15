0723004ab6484dd8ffe9e288ce9dedb2729bde25
bemova <david.bemova@gmail.com>
2020-06-15 19:08:56 -0400

    apply the pr

--- utility/adversarial_attack.py
+++ utility/adversarial_attack.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import torch
 from torch.autograd import Variable, grad
 from utility.utils import to_var, device
